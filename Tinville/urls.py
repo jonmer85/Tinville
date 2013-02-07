d0b8142a4dd3afe027ec48280f6e7fbf076a9f9a
jonmer85 <jonmer85@gmail.com>
2013-02-06 19:42:35 -0500

    Brought in HTML5 boilterplate starter code and have a default home page served up

--- Tinville/urls.py
+++ Tinville/urls.py
@@ -1,17 +1,26 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from Tinville.Site.views import home
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Tinville.views.home', name='home'),
-    # url(r'^Tinville/', include('Tinville.foo.urls')),
+    # Home Page -- Replace as you prefer
+    #url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', home),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+#    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+#    url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    if settings.MEDIA_ROOT:
+        urlpatterns += static(settings.MEDIA_URL,
+            document_root=settings.MEDIA_ROOT)
+    if settings.STATIC_ROOT:
+        urlpatterns += static(settings.STATIC_URL, 
+            document_root=settings.STATIC_ROOT)
