6187d72fd0d9a86e6a334307b21b061f36003d2d
jonmer85 <jonmer85@gmail.com>
2013-02-18 22:12:08 -0500

    Static content now served up in development environment

--- Tinville/settings.py
+++ Tinville/settings.py
@@ -2,7 +2,7 @@
 
 import os.path
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -165,3 +166,18 @@ LOGGING = {
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
+
+AWS_STORAGE_BUCKET_NAME = 'Tinville'
+
+DEFAULT_FILE_STORAGE = 'Tinville.Site.s3utils.MediaS3BotoStorage'
+STATICFILES_STORAGE = 'Tinville.Site.s3utils.StaticS3BotoStorage'
+
+AWS_ACCESS_KEY_ID = 'AKIAJJLBU23GKJZH6SQA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'tinville'
+
+S3_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATIC_DIRECTORY = '/static/'
+MEDIA_DIRECTORY = '/media/'
+STATIC_URL = S3_URL + STATIC_DIRECTORY
+MEDIA_URL = S3_URL + MEDIA_DIRECTORY
