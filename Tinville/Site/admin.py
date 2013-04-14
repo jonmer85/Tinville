449ddc6c21bed245d62b6300b61ce9db18c6a922
jonmer85 <jonmer85@gmail.com>
2013-04-13 23:29:32 -0400

    Start of custom TinvilleUser django model

---
+++ Tinville/Site/admin.py
@@ -0,0 +1,100 @@
+from django import forms
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from Tinville.Site.models import TinvilleUser
+
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = TinvilleUser
+        fields = ('email',
+                  'first_name',
+                  'last_name',
+                  'middle_name',
+                  'is_seller',
+                  'other_site_url',
+                  'shop_name'
+                )
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = TinvilleUser
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+
+
+class TinvilleUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email',
+                    'first_name',
+                    'middle_name',
+                    'last_name',
+                    'is_admin',
+                    'is_seller',
+                    'other_site_url',
+                    'shop_name'
+                    )
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'is_seller')}),
+        ('Personal info', {'fields': ('first_name', 'middle_name', 'last_name', 'other_site_url', 'shop_name')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'middle_name', 'last_name', 'other_site_url', 'shop_name', 'is_seller', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+# Now register the new UserAdmin...
+admin.site.register(TinvilleUser, TinvilleUserAdmin)
+# ... and, since we're not using Django's builtin permissions,
+# unregister the Group model from admin.
+#TODO should we unregister? Group permissions may help
+admin.site.unregister(Group)
--- Tinville/Site/models.py
+++ Tinville/Site/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+    )
 
 # Create your models here.
+
+
+class TinvilleUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.create_user(
+            email=TinvilleUserManager.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name
+            )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email,
+                                password=password,
+                                first_name=first_name,
+                                last_name=last_name
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class TinvilleUser(AbstractBaseUser):
+
+    email = models.EmailField(verbose_name='email address', unique=True, db_index=True, max_length=254)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    # Seller/Designer fields
+    is_seller = models.BooleanField(default=True)
+    other_site_url = models.URLField(verbose_name='Other Site URL', max_length=2083)
+    shop_name = models.CharField(verbose_name="Shop name", unique=True, db_index=True, max_length=100)
+
+
+    objects = TinvilleUserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.first_name + self.last_name
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.first_name
+
+    def __unicode__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        #TODO Add permission system for designer pages
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        #TODO Add permission system for designer pages
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+
+
+
+
+
