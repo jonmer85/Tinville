00b008c20e93ffba999c4de009842bf37cd6cfbf
jonmer85 <jonmer85@gmail.com>
2013-08-28 21:48:39 -0400

    Configuration fot new Heroku DB

--- Tinville/settings/base.py
+++ Tinville/settings/base.py
@@ -14,17 +14,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'Tinville',                      # Or path to database file if using sqlite3.
-        'USER': 'django_login',                      # Not used with sqlite3.
-        'PASSWORD': 'django_login',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 
 AUTH_USER_MODEL = 'user.TinvilleUser'
 
