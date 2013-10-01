792ceb3ac5bdd54459dd0ba8557364c164907a36
senay <senbay01@gmail.com>
2013-09-30 23:38:33 -0400

    importerror

--- Tinville/Site/views.py
+++ Tinville/Site/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+def faq(request):
+    return render_to_response("infopages/faq.html", {}, context_instance=RequestContext(request))
+def about(request):
+    return render_to_response("infopages/about.html", {}, context_instance=RequestContext(request))
+def policies(request):
+    return render_to_response("infopages/policies.html", {}, context_instance=RequestContext(request))
+def terms(request):
+    return render_to_response("infopages/terms.html", {}, context_instance=RequestContext(request))
 
