04440aa5dbd11a03fb6aca7b208c73c055118e88
senay <senbay01@gmail.com>
2013-10-10 20:54:26 -0400

    No reason for the "Site" section of the admin page.

--- Tinville/Site/views.py
+++ Tinville/Site/views.py
@@ -1,11 +1,2 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
-def faq(request):
-    return render_to_response("infopages/faq.html", {}, context_instance=RequestContext(request))
-def about(request):
-    return render_to_response("infopages/about.html", {}, context_instance=RequestContext(request))
-def policies(request):
-    return render_to_response("infopages/policies.html", {}, context_instance=RequestContext(request))
-def terms(request):
-    return render_to_response("infopages/terms.html", {}, context_instance=RequestContext(request))
