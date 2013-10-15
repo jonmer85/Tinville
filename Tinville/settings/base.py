2746593320fe7f3a2fc12b17d3519ddc949813f3
jbowman1107 <jeff.bowman@tinville.com>
2013-10-15 18:46:26 -0400

    Update base.py
    Added Jenkins_Tasks

--- Tinville/settings/base.py
+++ Tinville/settings/base.py
@@ -151,6 +151,12 @@ PROJECT_APPS = (
    'Tinville.user'
 )
 
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',
+)
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
