1e1a5d7fe5385f9f5bf9ada61074bf5594dd31a6
jon <jonmer85@gmail.com>
2014-01-13 20:59:06 -0500

    Start of the teaser page that we will use to promote the site until official Alpha/Beta/Launch

--- Tinville/urls.py
+++ Tinville/urls.py
@@ -7,22 +7,8 @@ from Tinville.user.forms import LoginForm, TinvilleShopperCreationForm, Tinville
 
 admin.autodiscover()
 
-urlpatterns = patterns('django.contrib.flatpages.views',
-    url(r'^about/$', 'flatpage',  kwargs={'url': '/about/'}, name='home_about'),
-    url(r'^faq/$', 'flatpage', kwargs={'url': '/faq/'}, name='home_faq'),
-    url(r'^policies/$', 'flatpage', kwargs={'url': '/policies/'}, name='home_policies'),
-    url(r'^terms/$', 'flatpage', kwargs={'url': '/terms/'}, name='home_terms'),
-)
-
-urlpatterns += patterns('',
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^register$', 'Tinville.user.views.register'),
-    url(r'^activate/(?P<activation_key>\w+)$', ActivationView.as_view(), name='activate-user'),
-    url(r'^notifications$', TemplateView.as_view(template_name='notification.html'), name='notifications'),
-    url(r'^ajax_login$', ajax_login,
-        {'template_name': 'login_form.html', 'authentication_form': LoginForm},
-        name='ajax_logins'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='teaser_home.html'), name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     #IMPORTANT!!! This route need to always be last since it consumes the entire namespace!
