ae800a1bf834f4c6b003d20c9d3315c5a76f26f6
jon <jonmer85@gmail.com>
2014-01-15 19:35:44 -0500

    Added the mailing list items to the admin page

--- Tinville/Site/admin.py
+++ Tinville/Site/admin.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.contrib.sites.models import Site
+from .models import MailingListItem
 
 class HiddenAdmin(admin.ModelAdmin):
     def get_model_perms(self, request):
@@ -8,6 +9,23 @@ class HiddenAdmin(admin.ModelAdmin):
         Return empty perms dict thus hiding the model from admin index.
         """
         return {}
+
+
+class MailingListItemAdmin(admin.ModelAdmin):
+
+    list_display = ('email',
+                    'ip_address',
+                    'creation_time'
+                    )
+
+    list_filter = ('email',)
+    search_fields = ('email', 'ip_address', 'creation_time')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+# Now register the new UserAdmin...
+admin.site.register(MailingListItem, MailingListItemAdmin)
 
 admin.site.unregister(Site)
 admin.site.register(Site, HiddenAdmin)
