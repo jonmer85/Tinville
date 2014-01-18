be8fdbbfcba28d6637ee94461c833a7bad56472d
jon <jonmer85@gmail.com>
2014-01-18 14:58:52 -0500

    - More lettuce tests for registration page

--- user/features/steps.py
+++ user/features/steps.py
@@ -10,51 +10,30 @@ from nose.tools import *
 def set_browser():
     world.browser = Client()
 
-@step(u'I access the URL "(,*)"')
-def access_url(step, url):
-    response = world.browser.get(url)
+@step(u'I access the registration page')
+def access_url(step):
+    response = world.browser.get('/register')
     assert_equals(200, response.status_code)
     world.dom = html.fromstring(response.content)
 
 @step(u'I see information to register as a designer')
 def see_information_to_register_as_a_designer(step):
-    designerInfo = world.dom.cssselect('#viewDesignerDetails')[0]
-    assert designerInfo.text.length > 0
+    assert_text_exists_in_first_element('#viewDesignerDetails', 'Designer info not found')
+
+@step(u'Or to register as a shopper')
+def see_information_to_register_as_a_designer(step):
+    assert_text_exists_in_first_element('#viewShopperDetails', 'Shopper info not found')
+
+@step(u'all registration forms are initially collapsed')
+def all_registration_forms_are_initially_collapsed(step):
+    assert_selector_does_not_exist('.in')
 
 
 
-# @step(u'When the shop is visited')
-# def when_the_shop_is_visited(step):
-#     response = world.browser.get(django_url(world.shop.get_absolute_url()))
-#     assert_equals(200, response.status_code)
-#     world.dom = html.fromstring(response.content)
-#
-# @step(u'Then the banner for the shop is displayed')
-# def then_the_banner_for_the_shop_is_displayed(step):
-#     assert_selector_contains('img.shopBanner', 'src', 'bar')
-#
-# @step(u'And the items for the shop are displayed')
-# def and_the_items_for_the_shop_are_displayed(step):
-#     assert_selector_exists('.shopItems')
-#
-# @step(u'And the Tinville header is displayed')
-# def and_the_tinville_header_is_displayed(step):
-#     assert_selector_exists('.navbar')
-#
-# @step(u'Then there should be (\d+) items displayed')
-# def then_there_should_be_n_items_displayed(step, n):
-#     assert_number_of_selectors('.shopItems .shopItem', int(n))
-#
-# @step(u'And every item should have a name')
-# def and_every_item_should_have_a_name(step):
-#     assert_text_of_every_selector('.shopItems .shopItem .name', 'itemname')
-#
-# @step(u'And every item should have an image')
-# def and_every_item_should_have_an_image(step):
-#     assert_every_selector_contains('.shopItems .shopItem img', 'src', 'itemimage')
-#
-# @step(u'And every item should have a price')
-# def and_every_item_should_have_a_price(step):
-#     assert_text_of_every_selector('.shopItems .shopItem .price', '$3.42')
+
+# Helper methods
+def assert_text_exists_in_first_element(selector, error_text):
+    first_element = world.dom.cssselect(selector)[0]
+    assert_true(len(html.tostring(first_element)) > 0, error_text)
 
 
