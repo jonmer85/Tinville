adf3183dc018c9295c345c268e19423d75782be2
jbowman1107 <jeff.bowman@tinville.com>
2014-02-15 11:36:59 -0500

    Fix case senstive email

--- Tinville/Site/forms.py
+++ Tinville/Site/forms.py
@@ -5,3 +5,8 @@ class MailingListItemForm(forms.ModelForm):
     class Meta:
         model = MailingListItem
 
+    def clean_email(self):
+        """ Verifies that this email is not already in use """
+        email = self.cleaned_data['email'].lower()  # Oh hey, this is important.
+        return email
+
