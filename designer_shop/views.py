1a1348f43c870e93d3c550e93c7620db50973fbe
jbowman1107 <jeff.bowman@tinville.com>
2014-03-01 14:24:28 -0500

    Beggining of shop editor shell

--- designer_shop/views.py
+++ designer_shop/views.py
@@ -5,3 +5,6 @@ def shopper(request, slug):
     return render(request, 'designer_shop/shopper.html', {
         'shop': get_object_or_404(Shop, slug__exact=slug)
     })
+
+def shopeditor(request):
+    return render(request, 'designer_shop/shopeditor.html')
