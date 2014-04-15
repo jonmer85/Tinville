df9cedc01b668b3bd1f76c8def8b90e93606fc64
jeff <jeff.bowman@tinville.com>
2014-04-15 19:09:53 -0400

    Shit got deleted.... Broke lettuce

--- Tinville/settings/base.py
+++ Tinville/settings/base.py
@@ -50,6 +50,10 @@ USE_TZ = True
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = PROJECT_DIR.child("media")
 
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -57,6 +61,9 @@ MEDIA_ROOT = PROJECT_DIR.child("media")
 # Example: "/home/media/media.lawrence.com/static/"
 STATIC_ROOT = ""
 
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
