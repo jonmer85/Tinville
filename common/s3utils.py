b90b4f002fd3ea26b1b244c0c68fee80290724e2
jon <jonmer85@gmail.com>
2014-05-17 18:40:46 -0400

    More attempts to fix s3 media

--- common/s3utils.py
+++ common/s3utils.py
@@ -1,4 +1,9 @@
 from storages.backends.s3boto import S3BotoStorage
 
-StaticS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaS3BotoStorage = lambda: S3BotoStorage(location='media')
+
+class MediaS3BotoStorage(S3BotoStorage):
+    location = 'media'
+
+
+class StaticS3BotoStorage(S3BotoStorage):
+    location = 'static'
