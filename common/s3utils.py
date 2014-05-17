482233615474f3a9ac3607012e8d9dffc3315a19
jon <jonmer85@gmail.com>
2014-05-17 19:06:28 -0400

    s3 media, take infinity?

--- common/s3utils.py
+++ common/s3utils.py
@@ -1,9 +1,20 @@
 from storages.backends.s3boto import S3BotoStorage
 
+class StaticRootS3BotoStorage(S3BotoStorage):
+    """
+    Storage for static files.
+    """
 
-class MediaS3BotoStorage(S3BotoStorage):
-    location = 'media'
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = 'static'
+        super(StaticRootS3BotoStorage, self).__init__(*args, **kwargs)
 
 
-class StaticS3BotoStorage(S3BotoStorage):
-    location = 'static'
+class MediaRootS3BotoStorage(S3BotoStorage):
+    """
+    Storage for uploaded media files.
+    """
+
+    def __init__(self, *args, **kwargs):
+        kwargs['location'] = 'media'
+        super(MediaRootS3BotoStorage, self).__init__(*args, **kwargs)
