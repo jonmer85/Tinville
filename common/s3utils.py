c2bf16474845f94e7b05023b0ac78a01ce43e19f
jon <jonmer85@gmail.com>
2014-05-17 18:29:56 -0400

    Added many gitignore settings and put back the media storage as it was

--- common/s3utils.py
+++ common/s3utils.py
@@ -1,6 +1,4 @@
 from storages.backends.s3boto import S3BotoStorage
 
 StaticS3BotoStorage = lambda: S3BotoStorage(location='static')
-
-class MediaS3BotoStorage(S3BotoStorage):
-    location = 'media'
+MediaS3BotoStorage = lambda: S3BotoStorage(location='media')
