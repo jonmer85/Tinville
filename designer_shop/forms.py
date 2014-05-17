067eb9855e96a59252640dc5868f8ac45de26f66
jeff <jeff.bowman@tinville.com>
2014-05-17 10:20:29 -0400

    Fixed tinville tag size for senai's shitty phone :)

--- designer_shop/forms.py
+++ designer_shop/forms.py
@@ -49,7 +49,7 @@ class ProductCreationForm(forms.ModelForm):
                          ,
                          css_class="accordion", css_id="accordion2"),
                 Submit('productCreationForm', 'Create', css_class='tinvilleButton'),
-                css_class="container"
+                css_class="container col-sm-12"
             )
 
         )
