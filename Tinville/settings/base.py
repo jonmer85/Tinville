5451461e1740ff1e3ef016cb89673e1e2d6e8eec
jon <jonmer85@gmail.com>
2014-05-21 22:04:25 -0400

    Now creating partners and stockrecords for products. These models are used to track pricing, quantity, and fulfillment partners (users)

--- Tinville/settings/base.py
+++ Tinville/settings/base.py
@@ -136,6 +136,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
+    'django.core.context_processors.request',
     'django.contrib.auth.context_processors.auth',
     'django.contrib.messages.context_processors.messages',
     'django_mobile.context_processors.flavour',
