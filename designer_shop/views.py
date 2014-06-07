df860c4cd4241238ecf23f8e3a376d2b35d01c79
afdabro <andrewfdabrowski@gmail.com>
2014-06-07 18:49:10 -0400

    Merge branch 'Sprint3' of https://github.com/jonmer85/Tinville into Sprint3Andy
    Conflicts:
    	designer_shop/views.py

--- designer_shop/views.py
+++ designer_shop/views.py
@@ -34,33 +34,37 @@ def shopper(request, slug):
         # 'categories': get_object_or_404(get_model('catalogue', 'AbstrastCategory')).objects.all()
     })
 
+
 def shopeditor(request, slug):
     shop = get_object_or_404(Shop, slug__iexact=slug)
-    form = None
-    if request.method == 'POST':
-        if request.POST.__contains__('bannerUploadForm'):
-            form = BannerUploadForm(request.POST, request.FILES)
-            if form.is_valid():
-                shop.banner = form.cleaned_data["banner"]
-                shop.save(update_fields=["banner"])
-            return renderShopEditor(request, shop, bannerUploadForm=form)
-        elif request.POST.__contains__('logoUploadForm'):
-            form = LogoUploadForm(request.POST, request.FILES)
-            if form.is_valid():
-                shop.logo = form.cleaned_data["logo"]
-                shop.save(update_fields=["logo"])
-            return renderShopEditor(request, shop, logoUploadForm=form)
-        else:
-            if request.method == 'POST':
-                sizeVariationType = request.POST["sizeVariation"]
-                sizes = get_sizes_colors_and_quantities(sizeVariationType, request.POST)
-                form = ProductCreationForm(request.POST, request.FILES, sizes=sizes)
+    if(user_shop_owner(request, shop)):
+        form = None
+        if request.method == 'POST':
+            if request.POST.__contains__('bannerUploadForm'):
+                form = BannerUploadForm(request.POST, request.FILES)
                 if form.is_valid():
-                    canonicalProduct = form.save(shop)
-                    form = ProductCreationForm()
-            return renderShopEditor(request, shop, productCreationForm=form)
+                    shop.banner = form.cleaned_data["banner"]
+                    shop.save(update_fields=["banner"])
+                return renderShopEditor(request, shop, bannerUploadForm=form)
+            elif request.POST.__contains__('logoUploadForm'):
+                form = LogoUploadForm(request.POST, request.FILES)
+                if form.is_valid():
+                    shop.logo = form.cleaned_data["logo"]
+                    shop.save(update_fields=["logo"])
+                return renderShopEditor(request, shop, logoUploadForm=form)
+            else:
+                if request.method == 'POST':
+                    sizeVariationType = request.POST["sizeVariation"]
+                    sizes = get_sizes_colors_and_quantities(sizeVariationType, request.POST)
+                    form = ProductCreationForm(request.POST, request.FILES, sizes=sizes)
+                    if form.is_valid():
+                        canonicalProduct = form.save(shop)
+                        form = ProductCreationForm()
+                return renderShopEditor(request, shop, productCreationForm=form)
+        else:
+            return renderShopEditor(request, shop)
     else:
-        return renderShopEditor(request, shop)
+        return redirect('home')
 
 def ajax_about(request, slug):
     if request.method == 'POST':
@@ -129,9 +133,53 @@ def get_sizes_colors_and_quantities(sizeType, post):
                     "colorsAndQuantities": []
                 }
 
-def renderShopEditor(request, shop, productCreationForm=None, aboutForm=None, colorPickerForm=None):
-    if user_shop_owner(request, shop) :
-    return render(request, 'designer_shop/shopeditor.html', {
+                j = 0
+                while (True):
+                    color = sizeDimensionTemplate + "_colorSelection" + str(j)
+                    quantity = sizeDimensionTemplate + "_quantityField" + str(j)
+                    if color in post and quantity in post:
+                        if post[color] and post[quantity]:
+                            sizes[i]["colorsAndQuantities"].append({"color": post[color], "quantity": post[quantity]})
+                    else:
+                        break
+                    j += 1
+                i += 1
+            else:
+                break
+        return sizes
+
+    if sizeType == SIZE_NUM:
+        sizes = {}
+        i = 0
+        while (True):
+            sizeNumberTemplate = "sizeNumberSelectionTemplate" + str(i)
+            sizeNumberSelection = sizeNumberTemplate + "_sizeNumberSelection"
+            if post[sizeNumberSelection]:
+                sizes[i] = {
+                    "sizeNum": post[sizeNumberSelection],
+                    "colorsAndQuantities": []
+                }
+
+                j = 0
+                while (True):
+                    color = sizeNumberTemplate + "_colorSelection" + str(j)
+                    quantity = sizeNumberTemplate + "_quantityField" + str(j)
+                    if color in post and quantity in post:
+                        if post[color] and post[quantity]:
+                            sizes[i]["colorsAndQuantities"].append({"color": post[color], "quantity": post[quantity]})
+                    else:
+                        break
+                    j += 1
+                i += 1
+            else:
+                break
+        return sizes
+
+
+def renderShopEditor(request, shop, productCreationForm=None, aboutForm=None, colorPickerForm=None, logoUploadForm=None,
+                     bannerUploadForm=None):
+    if(user_shop_owner(request, shop)):
+        return render(request, 'designer_shop/shopeditor.html', {
         'shop': shop,
         'productCreationForm': productCreationForm or ProductCreationForm,
         'bannerUploadForm': bannerUploadForm or BannerUploadForm(initial=
