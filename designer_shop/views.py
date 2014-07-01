1f4d87672f765f09265531875ec8872ad616b5b9
jeff <jeff.bowman@tinville.com>
2014-07-01 19:34:20 -0400

    fixed service

--- designer_shop/views.py
+++ designer_shop/views.py
@@ -116,7 +116,7 @@ def ajax_color(request, slug):
 def get_variants(item):
     variants = get_list_or_empty(Product, parent=item.id)
     colorsizequantitydict = collections.defaultdict(list)
-    addsizetype = collections.defaultdict(list)
+    addsizetype = collections.defaultdict(dict)
     for variant in variants:
         color = ""
         sizeSet = ""
@@ -148,7 +148,7 @@ def get_variants(item):
         variantsize = str(sizeSet) + str(sizeX) + divider + str(sizeY) + str(sizeNum)
         quantitysize = {'size': variantsize.capitalize(), 'quantity': quantity, 'price': price, 'currency': currency}
         colorsizequantitydict[str(color).capitalize()].append(quantitysize)
-    addsizetype[get_sizetype(variants)].append(colorsizequantitydict)
+    addsizetype= {get_sizetype(variants): colorsizequantitydict }
     return json.dumps(addsizetype)
 
 def get_sizetype(variants):
