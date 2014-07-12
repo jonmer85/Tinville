4bdf5afe866db3dc8439e614ac7bb5fc362fd8c1
jeff <jeff.bowman@tinville.com>
2014-07-12 14:41:17 -0400

    Re added delete item

--- designer_shop/views.py
+++ designer_shop/views.py
@@ -43,7 +43,6 @@ class IsShopOwnerDecorator(object):
         return self.authenticate(request, shop_slug, None)
 
 class IsShopOwnerDecoratorUsingItem(IsShopOwnerDecorator):
-
     def __call__(self, request, shop_slug, item_slug):
         return self.authenticate(request, shop_slug, item_slug)
 
@@ -322,3 +321,8 @@ def processShopEditorForms(request, shop_slug, item_slug=None):
     else:
         return renderShopEditor(request, shop, item=get_object_or_404(Product, slug__iexact=item_slug, parent__isnull=True) if item_slug else None)
 
+@IsShopOwnerDecoratorUsingItem
+def delete_product(request, shop_slug, item_slug):
+    item = get_object_or_404(Product, slug__iexact=item_slug, parent__isnull=True)
+    product = Product.objects.get(pk=item.id).delete()
+    return redirect('designer_shop.views.shopeditor', shop_slug)
