f85f478212d5fc64f5a97baeb34331ac432d35f5
Tommy Leedberg <xtommy.leedbergx@gmail.com>
2014-07-21 18:33:04 -0400

    tommy - multiimage stuff

--- designer_shop/forms.py
+++ designer_shop/forms.py
@@ -43,6 +43,7 @@ class ProductCreationForm(forms.ModelForm):
         self.fields['product_image1'] = forms.ImageField(required=False)
         self.fields['product_image2'] = forms.ImageField(required=False)
         self.fields['product_image3'] = forms.ImageField(required=False)
+        self.fields['product_image4'] = forms.ImageField(required=False)
 
         self.helper = FormHelper()
         self.helper.form_show_labels = False
@@ -59,7 +60,8 @@ class ProductCreationForm(forms.ModelForm):
                          Field( 'product_image', css_id="id_productImage" ),
                          Field( 'product_image1', css_id="id_productImage1"),
                          Field( 'product_image2', css_id="id_productImage2"),
-                         Field( 'product_image3', css_id="id_productImage3")
+                         Field( 'product_image3', css_id="id_productImage3"),
+                         Field( 'product_image4', css_id="id_productImage4")
                 ),
                 Fieldset('Sizes and Colors',
                          Field('sizeVariation', placeholder='Choose a variation'),
@@ -170,23 +172,38 @@ class ProductCreationForm(forms.ModelForm):
         # Jon M TBD - Right now we only use 1 product class - "Apparel"
         canonicalProduct.product_class = get_model('catalogue', 'ProductClass').objects.all()[:1].get()
         canonicalProduct.save()
-        productImage = ProductImage(product=canonicalProduct)
-        productImage.original = self.cleaned_data['product_image']
-        productImage.original = self.cleaned_data['product_image1']
-        productImage.original = self.cleaned_data['product_image2']
-        productImage.original = self.cleaned_data['product_image3']
-        productImage.save()
+
         canonicalId = canonicalProduct.id
         if is_edit:
             # Remove all variants since they will get recreated below
             get_model('catalogue', 'Product').objects.get(parent=canonicalId).delete()
 
 
-        if not is_edit:
-            # Tommy Leedberg TODO!!!! Make this work for editing images and remove if statement above!!!
-            productImage = ProductImage(product=canonicalProduct)
-            productImage.original = self.cleaned_data['product_image']
-            productImage.save()
+        #if not is_edit:
+        # Tommy Leedberg TODO!!!! Make this work for editing images and remove if statement above!!!
+        productImage = ProductImage(product=canonicalProduct)
+        productImage1 = ProductImage(product=canonicalProduct)
+        productImage2 = ProductImage(product=canonicalProduct)
+        productImage3 = ProductImage(product=canonicalProduct)
+        productImage4 = ProductImage(product=canonicalProduct)
+
+        productImage.display_order = 1
+        productImage1.display_order = 2
+        productImage2.display_order = 3
+        productImage3.display_order = 4
+        productImage4.display_order = 5
+
+        productImage.original = self.cleaned_data['product_image']
+        productImage1.original = self.cleaned_data['product_image1']
+        productImage2.original = self.cleaned_data['product_image2']
+        productImage3.original = self.cleaned_data['product_image3']
+        productImage4.original = self.cleaned_data['product_image4']
+
+        productImage.save()
+        productImage1.save()
+        productImage2.save()
+        productImage3.save()
+        productImage4.save()
 
         i = 0
         while True:
