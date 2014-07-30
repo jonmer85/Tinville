63e77a178b8cc1f7adc65ebd02871b98b7226755
afdabro <andrewfdabrowski@gmail.com>
2014-07-29 20:42:17 -0400

    Lettuce test for delete item

--- common/lettuce_utils.py
+++ common/lettuce_utils.py
@@ -118,6 +118,9 @@ def wait_for_element_with_css_selector_to_be_displayed(css_selector):
 def wait_for_element_with_class_to_be_displayed(class_name):
     WebDriverWait(world.browser, 10).until(lambda s: s.find_element_by_class_name(class_name).is_displayed())
 
+def wait_for_element_with_link_text_to_be_displayed(link_text):
+    WebDriverWait(world.browser, 10).until(lambda s: s.find_element_by_link_text(link_text).is_displayed())
+
 def assert_page_exist(url):
     c = Client()
     response = c.get(url, follow=True)
--- designer_shop/features/uploaditems_steps.py
+++ designer_shop/features/uploaditems_steps.py
@@ -13,7 +13,9 @@ from common.lettuce_utils import *
 
 @step(u'When I click the delete button for the product')
 def when_I_click_delete_button_product(step):
-    world.browser.find_element_by_css_selector("a[href='/Demo/edit/delete_product/TestSizeSetItem']").click()
+    world.browser.find_element_by_css_selector("#minMaxIcon").click()
+    wait_for_element_with_link_text_to_be_displayed("Delete")
+    world.browser.find_element_by_link_text("Delete").click()
 
 @step(u'Then the product is removed')
 def then_product_is_removed(step):
@@ -25,5 +27,5 @@ def then_product_is_removed(step):
 
 @step(u'And the shop editor refreshes in a minimized state')
 def and_shopEditor_refreshes_minimized(step):
-    assert world.browser.find_element_by_css_selector("#editorPanels").hasClass("glyphicon-chevron-up")
-    assert world.browser.find_element_by_css_selector("#editorPanels").isCollapsed == bool(True)
+    assert world.browser.find_element_by_css_selector("#minMaxIcon.glyphicon-chevron-up").is_displayed() == True, "Because chevron should be facing up"
+    assert len(world.browser.find_elements_by_css_selector("#editorPanels.collapse")) == 1, "Because the editor panel should be collapsed"
