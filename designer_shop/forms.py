1de3781d71161f0d36a53599b75952eb7ef9693a
Tommy Leedberg <xtommy.leedbergx@gmail.com>
2014-08-23 11:04:36 -0400

    Tommy Leedberg - Updating product images to ClearableFIleinputs

--- designer_shop/forms.py
+++ designer_shop/forms.py
@@ -78,13 +78,13 @@ class ProductCreationForm(forms.ModelForm):
             = forms.ImageField(required=False, initial=self.get_value_if_in_edit_mode('product_image', None),
                                widget=AdvancedFileInput)
         self.fields['product_image1'] = forms.ImageField(required=False, initial=self.get_value_if_in_edit_mode('product_image1', None),
-                                                         widget=AdvancedFileInput)
+                                                         widget=forms.ClearableFileInput)
         self.fields['product_image2'] = forms.ImageField(required=False, initial=self.get_value_if_in_edit_mode('product_image2', None),
-                                                         widget=AdvancedFileInput)
+                                                         widget=forms.ClearableFileInput)
         self.fields['product_image3'] = forms.ImageField(required=False, initial=self.get_value_if_in_edit_mode('product_image3', None),
-                                                         widget=AdvancedFileInput)
+                                                         widget=forms.ClearableFileInput)
         self.fields['product_image4'] = forms.ImageField(required=False, initial=self.get_value_if_in_edit_mode('product_image4', None),
-                                                         widget=AdvancedFileInput)
+                                                         widget=forms.ClearableFileInput)
 
         self.fields['description'].widget = TinyMCE()
         self.fields['price'].label = ""
