d9b8c914282be7ce756f0ef284b5ec22ac99bf38
jeff <jeff.bowman@tinville.com>
2014-08-30 11:30:56 -0400

    Fixed bug in test step

--- designer_shop/features/itemdetail_steps.py
+++ designer_shop/features/itemdetail_steps.py
@@ -70,7 +70,7 @@ def then_my_color_is(step, color):
 
 @step(u'When I try to select a size there are no options')
 def when_i_try_select_a_size_before_selecting_color(step):
-    amountofoptions = len(Select(world.browser.find_element_by_class("itemSizeSelection")).all_selected_options)
+    amountofoptions = len(Select(world.browser.find_element_by_id("itemSizeSelection")).all_selected_options)
     assert amountofoptions == 1, "there should only be one option in the select (Choose a size), but there is " + str(amountofoptions)
 
 @step(u'When I select the size (.*)')
