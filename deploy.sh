3e56377b6a78acf3efb380a1ad93e1bf252f12bf
jonmer85 <jonmer85@gmail.com>
2014-09-12 18:35:13 -0400

    Experimenting with shippable yaml file

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-if [ $BRANCH = "Sprint6ShippableDeployments" ] then git push -f git@heroku.com:tinville-testing.git Sprint6:master fi
+#!/bin/sh
+[[ $BRANCH != 'Sprint6ShippableDeployments' ]] && exit 0
+git push -f git@heroku.com:tinville-testing.git Sprint6:master
