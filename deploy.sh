8d61c6a99415c0b6c585dc511e3e44377e70fbec
jonmer85 <jonmer85@gmail.com>
2014-09-12 19:08:38 -0400

    Experimenting with shippable yaml file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-[[ $BRANCH != 'Sprint6ShippableDeployments' ]] && exit 0
+[[ $BRANCH != 'Sprint6' ]] && exit 0
 git push -f git@heroku.com:tinville-testing.git Sprint6:master
+./initDataNoInput qatest
+./qatest collectstatic --noinput
+
+git push -f git@heroku.com:tinville-dev.git Sprint6:master
+./initDataNoInput dev
+./dev collectstatic --noinput
