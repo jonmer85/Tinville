529916a62d1716f3e587b6f9b63703181b25b993
jonmer85 <jonmer85@gmail.com>
2014-10-07 17:49:43 -0400

    Start of splitting of order on a per designer basis

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from decimal import Decimal as D
+import logging
+import six
 
 from django.contrib import messages
 from django.core.urlresolvers import reverse
@@ -6,6 +7,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.conf import settings
 from django.db.models import get_model
+from django.utils.translation import ugettext as _
+
+from oscar.apps.checkout import signals
+from oscar.core.loading import get_class, get_classes
 
 from oscar.apps.checkout.views import PaymentDetailsView as CorePaymentDetailsView, IndexView as CoreIndexView
 from oscar.apps.shipping.methods import NoShippingRequired, Free
@@ -14,11 +19,20 @@ from oscar_stripe import facade, PAYMENT_METHOD_STRIPE, PAYMENT_EVENT_PURCHASE
 # Create your views here.
 from oscar_stripe.facade import Facade
 
+RedirectRequired, UnableToTakePayment, PaymentError \
+    = get_classes('payment.exceptions', ['RedirectRequired',
+                                         'UnableToTakePayment',
+                                         'PaymentError'])
+UnableToPlaceOrder = get_class('order.exceptions', 'UnableToPlaceOrder')
+
 
 SourceType = get_model('payment', 'SourceType')
 Source = get_model('payment', 'Source')
 Country = get_model('address', 'Country')
 ShippingAddress = get_model('order', 'ShippingAddress')
+
+# Standard logger for checkout events
+logger = logging.getLogger('oscar.checkout')
 
 
 class PaymentDetailsView(CorePaymentDetailsView):
@@ -104,6 +118,146 @@ class PaymentDetailsView(CorePaymentDetailsView):
         method = Free
 
         return method
+
+    def submit(self, user, basket, shipping_address, shipping_method,  # noqa (too complex (10))
+               order_total, payment_kwargs=None, order_kwargs=None):
+        """
+        Submit a basket for order placement.
+
+        The process runs as follows:
+
+         * Generate an order number
+         * Freeze the basket so it cannot be modified any more (important when
+           redirecting the user to another site for payment as it prevents the
+           basket being manipulated during the payment process).
+         * Attempt to take payment for the order
+           - If payment is successful, place the order
+           - If a redirect is required (eg PayPal, 3DSecure), redirect
+           - If payment is unsuccessful, show an appropriate error message
+
+        :basket: The basket to submit.
+        :payment_kwargs: Additional kwargs to pass to the handle_payment method
+        :order_kwargs: Additional kwargs to pass to the place_order method
+        """
+        if payment_kwargs is None:
+            payment_kwargs = {}
+        if order_kwargs is None:
+            order_kwargs = {}
+
+        # Taxes must be known at this point
+        assert basket.is_tax_known, (
+            "Basket tax must be set before a user can place an order")
+        assert shipping_method.is_tax_known, (
+            "Shipping method tax must be set before a user can place an order")
+
+        # Freeze the basket so it cannot be manipulated while the customer is
+        # completing payment on a 3rd party site.  Also, store a reference to
+        # the basket in the session so that we know which basket to thaw if we
+        # get an unsuccessful payment response when redirecting to a 3rd party
+        # site.
+        self.freeze_basket(basket)
+        self.checkout_session.set_submitted_basket(basket)
+
+        # We define a general error message for when an unanticipated payment
+        # error occurs.
+        error_msg = _("A problem occurred while processing payment for this "
+                      "order - no payment has been taken.  Please "
+                      "contact customer services if this problem persists")
+
+        signals.pre_payment.send_robust(sender=self, view=self)
+
+        # Generate an order number for the top level that includes orders from all the shops
+        top_level_order_number = self.generate_order_number(basket)
+        self.checkout_session.set_order_number(top_level_order_number)
+
+        items_by_shop = {}
+        order_total_by_shop = 0
+        for line in basket.all_lines():
+            if(line.product.shop not in items_by_shop):
+                items_by_shop[line.product.shop] = []
+            items_by_shop[line.product.shop].append(line.product)
+            order_total_by_shop += line.price_excl_tax
+
+
+
+            # We generate the order number first as this will be used
+            # in payment requests (ie before the order model has been
+            # created).  We also save it in the session for multi-stage
+            # checkouts (eg where we redirect to a 3rd party site and place
+            # the order on a different request).
+            order_number = self.generate_order_number(basket)
+
+            logger.info("Order #%s: beginning submission process for basket #%d",
+                        order_number, basket.id)
+
+            try:
+                return self.handle_order_placement(
+                    order_number, user, basket, shipping_address, shipping_method,
+                    order_total_by_shop, **order_kwargs)
+            except UnableToPlaceOrder as e:
+                # It's possible that something will go wrong while trying to
+                # actually place an order.  Not a good situation to be in as a
+                # payment transaction may already have taken place, but needs
+                # to be handled gracefully.
+                msg = six.text_type(e)
+                logger.error("Order #%s: unable to place order - %s",
+                             order_number, msg, exc_info=True)
+                self.restore_frozen_basket()
+                return self.render_preview(
+                    self.request, error=msg, **payment_kwargs)
+
+
+
+        try:
+            self.handle_payment(top_level_order_number, order_total, **payment_kwargs)
+        except RedirectRequired as e:
+            # Redirect required (eg PayPal, 3DS)
+            logger.info("Order #%s: redirecting to %s", top_level_order_number, e.url)
+            return HttpResponseRedirect(e.url)
+        except UnableToTakePayment as e:
+            # Something went wrong with payment but in an anticipated way.  Eg
+            # their bankcard has expired, wrong card number - that kind of
+            # thing. This type of exception is supposed to set a friendly error
+            # message that makes sense to the customer.
+            msg = six.text_type(e)
+            logger.warning(
+                "Order #%s: unable to take payment (%s) - restoring basket",
+                top_level_order_number, msg)
+            self.restore_frozen_basket()
+
+            # We assume that the details submitted on the payment details view
+            # were invalid (eg expired bankcard).
+            return self.render_payment_details(
+                self.request, error=msg, **payment_kwargs)
+        except PaymentError as e:
+            # A general payment error - Something went wrong which wasn't
+            # anticipated.  Eg, the payment gateway is down (it happens), your
+            # credentials are wrong - that king of thing.
+            # It makes sense to configure the checkout logger to
+            # mail admins on an error as this issue warrants some further
+            # investigation.
+            msg = six.text_type(e)
+            logger.error("Order #%s: payment error (%s)", top_level_order_number, msg,
+                         exc_info=True)
+            self.restore_frozen_basket()
+            return self.render_preview(
+                self.request, error=error_msg, **payment_kwargs)
+        except Exception as e:
+            # Unhandled exception - hopefully, you will only ever see this in
+            # development...
+            logger.error(
+                "Order #%s: unhandled exception while taking payment (%s)",
+                top_level_order_number, e, exc_info=True)
+            self.restore_frozen_basket()
+            return self.render_preview(
+                self.request, error=error_msg, **payment_kwargs)
+
+        signals.post_payment.send_robust(sender=self, view=self)
+
+        # If all is ok with payment, try and place order
+        logger.info("Order #%s: payment successful, placing order",
+                    top_level_order_number)
+
 
 class IndexView(CoreIndexView):
     template_name = 'gateway.html'
