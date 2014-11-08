02315b5335e2900de7064a1f02f69591882c0e50
jeff <jeff.bowman@tinville.com>
2014-11-08 15:46:26 -0500

    added test to verify user cannot register a shop name that a url already consumes

--- user/forms.py
+++ user/forms.py
@@ -61,7 +61,7 @@ class TinvilleUserCreationForm(forms.ModelForm):
         if shop_exists:
             raise forms.ValidationError('Shop name is already taken.')
         # if the shop name resolves to a view that is not the shop.. it is trying to use a url that we already use.. dont let this happen!
-        if resolve(urlparse.urlparse('/' + shop_name + '/')[2]).view_name != 'designer_shop.views.shopper':
+        if resolve(urlparse.urlparse('/' + shop_name.lower() + '/')[2]).view_name != 'designer_shop.views.shopper':
             raise forms.ValidationError('Not a valid shop name, please choose another')
         return shop_name
 
