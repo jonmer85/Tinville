697cf692f86583ed530c6f8c6248629489493d99
yori <sverdlin.y@gmail.com>
2014-11-15 10:36:07 -0500

    Confirmation Page, Regular+Mobile

--- customer/app.py
+++ customer/app.py
@@ -1,11 +1,10 @@
 
 from oscar.apps.customer.app import CustomerApplication as CoreCustomerApplication
 
-from user.views import register # Jon M TODO migrate the user stuff to this Oscar overridden app
-
+from .views import AddressUpdateView
 
 class CustomerApplication(CoreCustomerApplication):
-    register_view = register
-    # payment_details_view  = PaymentDetailsView
+
+    address_update_view = AddressUpdateView
 
 application = CustomerApplication()
