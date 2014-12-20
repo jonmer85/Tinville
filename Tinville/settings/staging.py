756ee35560ee40046ff60ea9bb5306d03e4f85ed
jonmer85 <jonmer85@gmail.com>
2014-12-19 22:31:10 -0500

    Trying to fix Shippable

--- Tinville/settings/staging.py
+++ Tinville/settings/staging.py
@@ -1,4 +1,4 @@
 from .base import *  # Start with base settings
 
-BROKER_URL=os.environ['REDISTOGO_URL'],
-CELERY_RESULT_BACKEND=os.environ['REDISTOGO_URL']
+BROKER_URL=os.environ.get('REDISTOGO_URL', None)
+CELERY_RESULT_BACKEND=os.environ.get('REDISTOGO_URL', None)
