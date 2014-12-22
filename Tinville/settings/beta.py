403b62667ac841cc2acd960788bc17e6aac1a16a
jonmer85 <jonmer85@gmail.com>
2014-12-22 13:07:27 -0500

    More migration towards env variables for secret information

--- Tinville/settings/beta.py
+++ Tinville/settings/beta.py
@@ -5,16 +5,16 @@ from .base import *  # Start with base settings
 import dj_database_url
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
-GOOGLE_ANALYTICS_TRACKING_ID = 'UA-47214215-2'
+GOOGLE_ANALYTICS_TRACKING_ID = get_env_variable('GOOGLE_ANALYTICS_TRACKING_ID')
 
-ALLOWED_HOSTS = ['tinville-alpha.herokuapp.com', 'www.alpha.tinville.com']
+ALLOWED_HOSTS = ['tinville-beta.herokuapp.com', 'www.beta.tinville.com']
 
 DEFAULT_FILE_STORAGE = 'common.s3utils.MediaS3BotoStorage'
 STATICFILES_STORAGE = 'common.s3utils.StaticS3BotoStorage'
 
-AWS_ACCESS_KEY_ID = 'AKIAJJLBU23GKJZH6SQA'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'tinville-alpha'
+AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_SECURE_URLS = False
 
 S3_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
--- Tinville/settings/qatest.py
+++ Tinville/settings/qatest.py
@@ -14,9 +14,9 @@ DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_
 DEFAULT_FILE_STORAGE = 'common.s3utils.MediaS3BotoStorage'
 STATICFILES_STORAGE = 'common.s3utils.StaticS3BotoStorage'
 
-AWS_ACCESS_KEY_ID = 'AKIAJJLBU23GKJZH6SQA'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'tinville-testing'
+AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_SECURE_URLS = False
 
 S3_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
