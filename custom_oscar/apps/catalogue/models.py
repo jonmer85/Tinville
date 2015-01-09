c13c66c9563e07422f06db1966274ef377838c62
jonmer85 <jonmer85@gmail.com>
2015-01-08 22:15:47 -0500

    Fixed problem with thank-you page by making overriding the primary_image method of Product to point to the images on the parent product

--- custom_oscar/apps/catalogue/models.py
+++ custom_oscar/apps/catalogue/models.py
@@ -14,4 +14,28 @@ class Product(AbstractProduct):
             'shop_slug': self.slug,
             'item_slug': self.shop.slug})
 
+    def primary_image(self):
+        """
+        Returns the primary image for a product. Usually used when one can
+        only display one product image, e.g. in a list of products.
+        Jon M - Overriding this to get images from the parent product
+        """
+        images = self.images.all() if self.is_parent else self.parent.images.all()
+        ordering = self.images.model.Meta.ordering if self.is_parent else self.parent.images.model.Meta.ordering
+        if not ordering or ordering[0] != 'display_order':
+            # Only apply order_by() if a custom model doesn't use default
+            # ordering. Applying order_by() busts the prefetch cache of
+            # the ProductManager
+            images = images.order_by('display_order')
+        try:
+            return images[0]
+        except IndexError:
+            # We return a dict with fields that mirror the key properties of
+            # the ProductImage class so this missing image can be used
+            # interchangeably in templates.  Strategy pattern ftw!
+            return {
+                'original': self.get_missing_image(),
+                'caption': '',
+                'is_missing': True}
+
 from oscar.apps.catalogue.models import *
