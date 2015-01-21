00b54f63e9ee74ecc95efea12a14c3c7501bd9c7
afdabro <andrewfdabrowski@gmail.com>
2015-01-20 21:57:59 -0500

    Sends new order email to all designers in the customer's order

--- custom_oscar/apps/checkout/mixins.py
+++ custom_oscar/apps/checkout/mixins.py
@@ -1,33 +1,39 @@
 import logging
 from django.contrib.sites.models import get_current_site
 from oscar.core.loading import get_model, get_class
-from Tinville.user.models import TinvilleUser
+from user.models import TinvilleUser
+from common.utils import ExtractDesignerIdFromOrderId
 
 CommunicationEventType = get_model('customer', 'CommunicationEventType')
 Dispatcher = get_class('customer.utils', 'Dispatcher')
+Order = get_model('order', 'Order')
 
-logger = logging.getLogger('oscar.customer')
+logger = logging.getLogger('oscar.checkout')
 
 
 class SendOrderMixin(object):
-    communication_type_code = 'ORDER_PLACED'
+    communication_type_code = 'DESIGNER_ORDER_PLACED'
 
-    def send_new_order_email(self, order):
+    def send_new_order_email(self, top_level_order):
         code = self.communication_type_code
-        user = TinvilleUser.objects.get_by_natural_key('andrewfdabrowski@gmail.com')
-        messages = CommunicationEventType.objects.get_and_render(
-            code, self.get_message_context(user, order))
-        if messages and messages['body']:
-            Dispatcher().dispatch_user_messages(messages)
+
+        orders = Order.objects.filter(number__contains="-"+str(top_level_order.number))
+        for order in orders:
+            designerId = ExtractDesignerIdFromOrderId(order.number)
+            user = TinvilleUser.objects.get(id=designerId)
+            #Loads DESIGNER_ORDER_PLACED from DB fixture
+            messages = CommunicationEventType.objects.get_and_render(
+                code, self.get_message_context(user, order))
+
+            if messages and messages['body']:
+                dispatcher = Dispatcher(logger)
+                dispatcher.dispatch_user_messages(user, messages)
 
     def get_message_context(self, user, order):
         ctx = {
             'user': user,
             'order': order,
-
             'lines': order.lines.all()
         }
-
-        #'site': get_current_site(self.request),
 
         return ctx
