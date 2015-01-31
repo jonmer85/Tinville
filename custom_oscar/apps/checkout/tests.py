bc1a24ae95b83a1a2daa384fb8ac3563d55f2916
afdabro <andrewfdabrowski@gmail.com>
2015-01-31 13:21:23 -0500

    Update unit test

--- custom_oscar/apps/checkout/tests.py
+++ custom_oscar/apps/checkout/tests.py
@@ -23,4 +23,4 @@ class SendOrderMixinTests(TestCase):
         self.assertEqual(email.user.email, self.user.email)
         self.assertEqual(email.subject, "New Customer Order Placed", "Because the subject should be equal")
         self.assertEqual(email.body_text, "Hello, /n You have a new order available.", "Because the text body should be equal")
-        self.assertEqual(email.body_html, "<html><div>Hello,</div><p>A new order {{ order.number }} is available.</p><p>The order contains:</p><ul>{% for line in order.lines.all %}<li>{{ line.title }} - quantity: {{ line.quantity }}</li>{% endfor %}</ul><p></html>", "Because the html body should be equal")
+        self.assertEqual(email.body_html, "<html><div>Hello,</div><p>A new order 2-10011 is available.</p><p>The order contains:</p><ul><li>Dummy title - quantity: 1</li></ul><p></html>", "Because the html body should be equal")
