ef439e99fe503a38cab6274cd633dc932853888f
jonmer85 <jonmer85@gmail.com>
2015-02-07 08:20:47 -0500

    Migration fixes

--- custom_oscar/apps/catalogue/migrations/0002_product_shop.py
+++ custom_oscar/apps/catalogue/migrations/0002_product_shop.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import image_cropping.fields
 
 
 class Migration(migrations.Migration):
@@ -17,12 +16,6 @@ class Migration(migrations.Migration):
             model_name='product',
             name='shop',
             field=models.ForeignKey(default=None, to='designer_shop.Shop'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='productimage',
-            name=b'cropping',
-            field=image_cropping.fields.ImageRatioField(b'original', '400x500', allow_fullsize=False, free_crop=False, adapt_rotation=False, box_max_width=400, size_warning=False, help_text=None, box_max_height=500, verbose_name='cropping', hide_image_field=False),
             preserve_default=True,
         ),
     ]
