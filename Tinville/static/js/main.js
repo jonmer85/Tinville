000d25974f2e67dcaf096643bcf68c1f1e64f973
afdabro <andrewfdabrowski@gmail.com>
2015-02-14 14:04:59 -0500

    fixes #481 Mobile warning when using shop editor adds session storage

--- Tinville/static/js/main.js
+++ Tinville/static/js/main.js
@@ -348,11 +348,3 @@ function RemoveCartItem(itemId)
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         stickyFooter();
     });
-
-        $(document).ready(function()
-        {
-            var mode = checkMode();
-            if(mode == 'xs' || mode == 'sm') {
-                $('#mobileModal').modal('show');
-            }
-        });
