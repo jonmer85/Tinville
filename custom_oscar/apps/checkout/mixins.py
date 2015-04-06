e50fecaee6ac4380bca1af75d1e71c1693b6b9f1
jonmer85 <jonmer85@gmail.com>
2015-04-05 21:55:23 -0400

    Fixed 589 - Order email (Added a link to the order)

--- custom_oscar/apps/checkout/mixins.py
+++ custom_oscar/apps/checkout/mixins.py
@@ -1,5 +1,6 @@
 import logging
 from django.contrib.sites.models import get_current_site
+from django.core.urlresolvers import reverse
 from oscar.core.loading import get_model, get_class
 from user.models import TinvilleUser
 from common.utils import ExtractDesignerIdFromOrderId
@@ -14,7 +15,7 @@ logger = logging.getLogger('oscar.checkout')
 class SendOrderMixin(object):
     communication_type_code = 'DESIGNER_ORDER_PLACED'
 
-    def send_new_order_email(self, top_level_order):
+    def send_new_order_email(self, top_level_order, request):
         code = self.communication_type_code
 
         orders = Order.objects.filter(number__contains="-"+str(top_level_order.number))
@@ -23,17 +24,19 @@ class SendOrderMixin(object):
             user = TinvilleUser.objects.get(id=designerId)
             #Loads DESIGNER_ORDER_PLACED from DB fixture
             messages = CommunicationEventType.objects.get_and_render(
-                code, self.get_message_context(user, order))
+                code, self.get_message_context(user, order, request))
 
             if messages and messages['body']:
                 dispatcher = Dispatcher(logger)
                 dispatcher.dispatch_user_messages(user, messages)
 
-    def get_message_context(self, user, order):
+    def get_message_context(self, user, order, request):
         ctx = {
             'user': user,
             'order': order,
-            'lines': order.lines.all()
+            'lines': order.lines.all(),
+            'url': request.build_absolute_uri(reverse('dashboard:order-detail',
+                      kwargs={'number': order.number}))
         }
 
         return ctx
