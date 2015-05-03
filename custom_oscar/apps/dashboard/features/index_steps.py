feda2350d4d9d8693eaf81547492e3546e78c8af
jeff <jeff.bowman@tinville.com>
2015-05-03 11:09:54 -0400

    Merge branches 'Sprint8' and 'Sprint8_DashboardIndexLettuce' of https://github.com/jonmer85/Tinville into Sprint8_DashboardIndexLettuce
    Conflicts:
    	Tinville/lettucedb.sqlite3

--- custom_oscar/apps/dashboard/features/index_steps.py
+++ custom_oscar/apps/dashboard/features/index_steps.py
@@ -26,7 +26,7 @@ def i_am_on_the_dashboard_index_page(step):
 
 @step("I should see the '(.*)' table")
 def i_should_see_the_table(step, tabletitle):
-    tableheaders = len(world.browser.find_elements_by_xpath("//table/caption[contains(text(), '" + tabletitle + "')]")) \
+    tableheaders = len(world.browser.find_elements_by_xpath("//table/caption[normalize-space(.)='" + tabletitle + "']")) \
                     + len(world.browser.find_elements_by_xpath("//div[contains(@class, 'table-header') and normalize-space(.)='" + tabletitle + "']"))
 
     assert_equals(1, tableheaders, "Should be 1 " + tabletitle + " table but there was " + str(tableheaders))
@@ -38,7 +38,7 @@ def i_should_see_the_table(step, tabletitle):
         pass
 
     try:
-        table = world.browser.find_element_by_xpath("//table/caption[contains(text(), '" + tabletitle + "')]").find_element_by_xpath("..")
+        table = world.browser.find_element_by_xpath("//table/caption[normalize-space(.)='" + tabletitle + "']").find_element_by_xpath("..")
         type = "tr"
     except NoSuchElementException:
         pass
@@ -49,5 +49,11 @@ def i_should_see_the_table(step, tabletitle):
 @step("I should see my '(.*)'")
 def i_should_see_my_section(step, section):
     table = step.scenario.context["table"]
-    assert_equals(1, len(filter(lambda e: e.text == section, table.find_elements_by_css_selector(step.scenario.context["type"]))),
+
+    testlist = table.find_elements_by_css_selector(step.scenario.context["type"])
+
+    for listitem in testlist:
+        hello = listitem.text
+        hmm = 1
+    assert_equals(1, len(filter(lambda e: section in e.text, table.find_elements_by_css_selector(step.scenario.context["type"]))),
                   "No element with the text " + section + " exists in the table")
