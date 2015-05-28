8d52ed41ae5f143aaaf309c77c3e72e2abfcca6b
jonmer85 <jonmer85@gmail.com>
2015-05-28 14:41:22 -0400

    Added deployment for tinville.com from the master branch

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
-if [ $BRANCH != 'develop' ]; then exit 0; fi
 if [ $PULL_REQUEST != 'false' ]; then exit 0; fi
-git push -f git@heroku.com:tinville-testing.git develop:master
-heroku run ./initDataNoInput qatest --app tinville-testing
-heroku run ./qatest collectstatic --noinput --app tinville-testing
-heroku run ./qatest compress --force --app tinville-testing
 
-git push -f git@heroku.com:tinville-dev.git develop:master
-heroku run ./initDataNoInput dev --app tinville-dev
-heroku run ./dev collectstatic --noinput --app tinville-dev
-heroku run ./dev compress --force --app tinville-dev
+if [ $BRANCH == 'develop' ]; then 
+  git push -f git@heroku.com:tinville-testing.git develop:master
+  heroku run ./initDataNoInput qatest --app tinville-testing
+  heroku run ./qatest collectstatic --noinput --app tinville-testing
+  heroku run ./qatest compress --force --app tinville-testing
+
+  git push -f git@heroku.com:tinville-dev.git develop:master
+  heroku run ./initDataNoInput dev --app tinville-dev
+  heroku run ./dev collectstatic --noinput --app tinville-dev
+  heroku run ./dev compress --force --app tinville-dev
+else
+  if [ $BRANCH == 'master' ]; then 
+    git push -f git@heroku.com:tinville-beta.git master:master
+    heroku run ./initDataNoInput beta --app tinville-beta
+    heroku run python manage.py collectstatic --settings=Tinville.settings.beta --noinput --app tinville-beta
+    heroku run python manage.py compress --settings=Tinville.settings.beta --force --app tinville-beta
+  fi
+fi
+
+
