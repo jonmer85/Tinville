9d18ad558e155b90ae36b2d56b76a197dedb648c
snicka <senbay01@yahoo.com>
2015-06-08 20:43:40 -0400

    tweat of homeagain fixed item detail in tablet and mobile

--- Tinville/views.py
+++ Tinville/views.py
@@ -4,11 +4,39 @@ from designer_shop.views import get_filtered_products,get_category_products,get_
 from designer_shop.models import Shop,FeaturedShop
 from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from common.utils import get_list_or_empty, get_or_none
-
-
+from django.views.generic import ListView
 
 def home_gallery(request):
     template = "home.html"
+    page_template = "designer_shop/all_gallery.html"
+    products = get_filtered_products()
+    featured_shop = get_list_or_empty(FeaturedShop)
+    featured_shop_ids = [f.featured_id for f in featured_shop]
+    fshops = Shop.objects.filter(id__in=featured_shop_ids)
+    shopCategories, shopCategoryNames = get_filter_lists().categorylist()
+    menproducts = get_category_products(genderfilter="Men")
+    womenproducts = get_category_products(genderfilter="Women")
+
+
+
+
+    context = {
+        'products': products,
+        'menproducts': menproducts,
+        'womenproducts': womenproducts,
+        'fshops' : fshops,
+        'shopcategories': shopCategoryNames,
+        'shopgenders': get_filter_lists().genderlist(),
+        'shopProductCount' : len(products)
+
+    }
+    if request.is_ajax():
+        template = page_template
+    return render_to_response(template, context, context_instance=RequestContext(request))
+
+
+def shop_gallery(request):
+    template = "homepage/all_home.html"
     page_template = "designer_shop/all_gallery.html"
     products = get_filtered_products()
     featured_shop = get_list_or_empty(FeaturedShop)
