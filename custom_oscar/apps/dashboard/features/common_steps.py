613373e31e65e27f23b56bee3ad00d35ba9c1bd6
jon <jonmer85@gmail.com>
2015-06-20 16:01:18 -0400

    Added basic lettuce test for My Account and Order History

--- custom_oscar/apps/dashboard/features/common_steps.py
+++ custom_oscar/apps/dashboard/features/common_steps.py
@@ -2,30 +2,10 @@ from common.factories import create_order, create_product, create_basket_with_pr
 from django.contrib.auth.models import Permission
 from custom_oscar.apps.catalogue.models import Product
 from custom_oscar.apps.order.models import Order
-from user.models import TinvilleUser as User
 from lettuce import step
 from common.lettuce_utils import *
 
-@step("My user has correct permissions")
-def my_user_has_correct_permissions(step):
-    user = User.objects.get(pk=2)
-    dashboard_access_perm = Permission.objects.get(
-                codename='dashboard_access', content_type__app_label='partner')
-    user.user_permissions.add(dashboard_access_perm)
-    user.save()
-    world.browser.get(lettuce.django.get_server().url('/'))
-    sign_in("demo@user.com", "tinville")
-
 @step("I have some basic dashboard data")
 def i_have_some_basic_dashboar_data(step):
-    my_user_has_correct_permissions(step)
-    product_list = []
-    product_list.append(Product.objects.get(pk=2))
-    product_list.append(Product.objects.get(pk=3))
-    basket1 = create_basket_with_products(product_list)
-    basket2 = create_basket_with_products(product_list)
-    basket3 = create_basket_with_products(product_list)
-    create_order(number="1-10002", basket=basket1, user=User.objects.get(pk=2), shop=Shop.objects.get(pk=1))
-    create_order(number="1-10003", basket=basket2, user=User.objects.get(pk=2), shop=Shop.objects.get(pk=1))
-    create_order(number="1-10004", basket=basket3, user=User.objects.get(pk=2), shop=Shop.objects.get(pk=1), status="Shipped")
+    setup_basic_order_data()
 
