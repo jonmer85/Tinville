42b5b9e010ddbfa764fd2a043d73ac38ed040cf0
jon <jonmer85@gmail.com>
2015-06-26 13:51:09 -0400

    HOpefully really fixed build

--- Tinville/settings/beta.py
+++ Tinville/settings/beta.py
@@ -66,9 +66,10 @@ CACHES = {
 }
 
 # List of callables that know how to import templates from various sources.
+# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     ('django.template.loaders.cached.Loader', (
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
-    ))
+    )),
 )
