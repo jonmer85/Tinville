2bd22a8608a67cea755c0e3d8f6f7312b6503580
snicka <senbay01@yahoo.com>
2015-07-06 00:11:24 -0400

    new register flow and icons

--- Tinville/views.py
+++ Tinville/views.py
@@ -1,6 +1,6 @@
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response
-from designer_shop.views import get_filtered_products,get_category_products,get_filter_lists, get_categoryName
+from designer_shop.views import get_filtered_products,get_category_products,get_filter_lists, get_types
 from designer_shop.models import Shop,FeaturedShop
 from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from common.utils import get_list_or_empty, get_or_none, passes_test_cache
@@ -31,19 +31,22 @@ def home_gallery(request):
 def shop_gallery(request):
     template = "homepage/all_home.html"
     page_template = "designer_shop/all_gallery.html"
-    products = get_filtered_products().order_by('?')
-    shopCategories, shopCategoryNames = get_filter_lists().categorylist()
+
 
 
     if request.method == 'GET':
         if request.GET.__contains__('genderfilter'):
             products = get_filtered_products(post=request.GET, filter=True)
-            return render(request, 'designer_shop/shop_items.html', {
+            shopCategoryNames = get_types(request=request,shop_slug=None,group_by=request.GET['genderfilter'])
+            return render(request, 'homepage/all_home.html', {
                 'homemode': True,
                 'products': products,
                 'shopProductCount': len(products),
                 'shopcategories': shopCategoryNames
             })
+
+    products = get_filtered_products().order_by('?')
+    shopCategories, shopCategoryNames = get_filter_lists().categorylist()
     context = {
         'homemode': True,
         'products': products,
