9389fbaaa015aee92f5f1aa84398d6a0bcb848bd
jon <jonmer85@gmail.com>
2015-07-06 17:06:38 -0400

    Changed to using redis cloud

--- Tinville/settings/perf.py
+++ Tinville/settings/perf.py
@@ -47,7 +47,7 @@ COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
+redis_url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL', 'redis://localhost:6959'))
 CACHES = {
     'default': {
         'BACKEND': 'redis_cache.RedisCache',
