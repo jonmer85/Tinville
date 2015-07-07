940531e4b90d439b5f768eb0a145af82beb9179e
snicka <senbay01@yahoo.com>
2015-07-07 12:27:56 -0400

    default color revert thank-out

--- designer_shop/forms.py
+++ designer_shop/forms.py
@@ -374,7 +374,7 @@ class AboutBoxForm(forms.ModelForm):
 
 class DesignerShopColorPicker(forms.Form):
 
-    color = forms.CharField(widget=widgets.FarbtasticColorPicker, initial = "#5B595A", max_length=7, min_length = 6,
+    color = forms.CharField(widget=widgets.FarbtasticColorPicker, initial ="#5B595A", max_length=7, min_length = 6,
                              validators=[
         RegexValidator(
             regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
