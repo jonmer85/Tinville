c56a48ca3832bd5b189e45469828d96e5e0f8c41
jon <jonmer85@gmail.com>
2015-07-08 17:25:41 -0400

    Overwrite manifest

--- common/s3utils.py
+++ common/s3utils.py
@@ -22,22 +22,8 @@ class MediaS3BotoStorage(S3BotoStorage):
         kwargs['querystring_auth'] = False
         super(MediaS3BotoStorage, self).__init__(*args, **kwargs)
 
-class CompressorS3BotoStorage(S3BotoStorage):
-    def __init__(self, *args, **kwargs):
-        kwargs['location'] = 'compressor'
-        super(CompressorS3BotoStorage, self).__init__(*args, **kwargs)
-        self.local_storage = get_storage_class(
-            'compressor.storage.CompressorFileStorage')()
-
-    def save(self, name, content):
-
-        name = super(CompressorS3BotoStorage, self).save(name, content)
-        self.local_storage._save(name, content)
-        return name
+class CompressorS3BotoStorage(StaticS3BotoStorage):
 
     def get_available_name(self, name):
-        """ Overwrite existing file with the same name. """
-        if FILE_OVERWRITE or name == 'manifest.json':
-            name = self._clean_name(name)
-            return name
-        return super(CompressorS3BotoStorage, self).get_available_name(name)
+        name = self._clean_name(name)
+        return name
