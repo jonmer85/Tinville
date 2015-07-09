4e6584d8f2907395a76eae0f7b3417f8a817295b
snicka <senbay01@yahoo.com>
2015-07-09 06:07:45 -0400

    test

--- user/features/steps.py
+++ user/features/steps.py
@@ -19,7 +19,7 @@ def access_home_url(step):
 
 @step(u'(?:When|And) I register for a shopper account with email "([^"]*)" and password "([^"]*)"')
 def when_i_register_for_a_shopper_account_with_email_and_password(step, email, password):
-    form = fill_in_user_form(email=email, password=password)
+    form = fill_in_user_form(email=email, password=password, customer=True)
     submit_form_and_activate_user(form)
 
 @step(u'(?:When|And) I register but not activate a shopper account with email "([^"]*)" and password "([^"]*)"')
@@ -29,7 +29,7 @@ def when_i_register_but_not_activate_a_shopper_account_with_email_and_password(s
 
 @step(u'(?:When|And) I try to again register for a shopper account with email "([^"]*)" and password "([^"]*)"')
 def when_i_register_for_a_shopper_account_with_email_and_password(step, email, password):
-    form = fill_in_user_form(email=email, password=password, customer=True)
+    form = fill_in_user_form(email=email, password=password)
     submit_form_and_activate_user(form, False)
 
 @step(u'(?:When|And) I register for a shop named "([^"]*)"')
