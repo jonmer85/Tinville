a2fd88f1fb85238f97ed51f52e214108fdde1ffb
afdabro <andrewfdabrowski@gmail.com>
2015-12-29 20:44:19 -0500

    promoter link generation for twitter and facebook

--- Tinville/static/js/main.js
+++ Tinville/static/js/main.js
@@ -372,3 +372,63 @@ function RemoveCartItem(itemId)
         stickyFooter();
     });
 
+        function PromoteClick(promoterCode)
+        {
+            var currentLocation = window.location;
+            var currentQuery = currentLocation.params();
+            currentQuery.promoter = promoterCode;
+            currentLocation.params(currentQuery);
+            console.log(currentLocation);
+        }
+
+        function PromoteTwitterClick(promoterCode)
+        {
+            PromoteClick(promoterCode);
+            var twitterText = "Tinville";
+            var twitterHref = "http://twitter.com/share?text=" + twitterText + "&url=" + window.location;
+            $("#twitter-promote").attr('href',twitterHref);
+            $("#twitter-promote")[0].click();
+        }
+
+        function PromoteFacebookClick(promoterCode)
+        {
+            PromoteClick(promoterCode);
+            var facebookHref = "http://www.facebook.com/sharer.php?u=" + window.location;
+            $("#facebook-promote").attr('href',facebookHref);
+            $("#facebook-promote")[0].click();
+        }
+
+        location.params = function(params) {
+            var obj = {}, i, parts, len, key, value;
+
+            if (typeof params === 'string') {
+                value = location.search.match(new RegExp('[?&]' + params + '=?([^&]*)[&#$]?'));
+                return value ? value[1] : undefined;
+            }
+
+            var _params = location.search.substr(1).split('&');
+
+            for (i = 0, len = _params.length; i < len; i++) {
+                parts = _params[i].split('=');
+                if (! parts[0]) {continue;}
+                obj[parts[0]] = parts[1] || true;
+            }
+
+            if (typeof params !== 'object') {return obj;}
+
+            for (key in params) {
+                value = params[key];
+                if (typeof value === 'undefined') {
+                    delete obj[key];
+                } else {
+                    obj[key] = value;
+                }
+            }
+
+            parts = [];
+            for (key in obj) {
+                parts.push(key + (obj[key] === true ? '' : '=' + obj[key]));
+            }
+
+            location.search = parts.join('&');
+        };
